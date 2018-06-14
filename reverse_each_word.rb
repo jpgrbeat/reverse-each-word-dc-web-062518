f3e991b51e0bcde43d7a115a633433681c0f684e
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 18:59:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array2 = []
-  array.each do |word|
-    array2.push(word.reverse) 
-  end
-  string = array2.join(" ")
+  array.collect{array[i].reverse}
+  string = array.join(" ")
   string
 end
 
